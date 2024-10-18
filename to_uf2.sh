93a530262161fccc5db6df4441360a8f65b18e39
Tanaka <ex@com>
2024-10-18 23:24:03 +0000

    to_uf2.shを修正

--- to_uf2.sh
+++ to_uf2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-TRG=target/thumbv6m-none-eabi/release/baker-link
-OUT=release/baker-link
+TRG=target/thumbv6m-none-eabi/release/baker-link-dev
+OUT=release/baker-link-dev
 
 rm $OUT
 elf2uf2-rs $TRG $OUT
